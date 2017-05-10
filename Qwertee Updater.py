ec4c03d8bd639967692f62a1ce6cdf70134d2b6a
daf276 <daf276@gmx.de>
2017-05-10 18:14:50 +0200

    -Now Borderless -Now opens the window on the second monitor of two FullHD screens by default

--- Qwertee Updater.py
+++ Qwertee Updater.py
@@ -5,27 +5,26 @@ import io
 from PIL import Image
 from PIL import ImageTk
 
-
 def request_htmlbody():
     request = urllib.request.Request("https://www.qwertee.com/", headers={'User-Agent': "Magic Browser"})
     return urllib.request.urlopen(request).read().decode("utf-8").replace('\r', '').replace('\n', '')
 
 
-def find_images(html_body):
-    div_pattern = re.compile(r'<div class="mens-dynamic-image design-dynamic-image.*?</div>')
-    divs = div_pattern.findall(html_body)
+def find_image_tags(html_body):
+    divs = re.compile(r'<div class="mens-dynamic-image design-dynamic-image.*?</div>').findall(html_body)
 
     img_src_pattern = re.compile(r'<img src=".*?">')
 
-    images_tags = [ ]
+    images_tags = []
 
     for div in divs:
         images_tags.append(img_src_pattern.findall(div)[0])
+
     return images_tags
 
 
 def strip_img_urls(images):
-    return [s.replace(r'<img src="', '',).replace(r'">', '') for s in images]
+    return ["https:" + s.replace(r'<img src="', '',).replace(r'">', '') for s in images]
 
 
 def get_imagetk_from_url(url):
@@ -34,16 +33,22 @@ def get_imagetk_from_url(url):
     return ImageTk.PhotoImage(Image.open(io.BytesIO(raw_data)))
 
 
+def close_window(event):
+    root.destroy()
+
+
 html_body = request_htmlbody()
-image_urls = find_images(html_body)
-image_urls = strip_img_urls(image_urls)
+image_urls = strip_img_urls(find_image_tags(html_body))
 
 root = tkinter.Tk()
 root.title("Qwertee shirts")
+root.bind('<Escape>', close_window)
+root.overrideredirect(1)
+root.geometry("+1920+0")  # Opens the window on the top left corner of the second monitor with two FullHD Screens
 
-img = get_imagetk_from_url("https:" + image_urls[0])
-img2 = get_imagetk_from_url("https:" + image_urls[1])
-img3 = get_imagetk_from_url("https:" + image_urls[2])
+img = get_imagetk_from_url(image_urls[0])
+img2 = get_imagetk_from_url(image_urls[1])
+img3 = get_imagetk_from_url(image_urls[2])
 
 photo_label = tkinter.Label(image=img)
 photo_label2 = tkinter.Label(image=img2)
